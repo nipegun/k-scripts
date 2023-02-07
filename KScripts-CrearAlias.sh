b3af82dc1da299b676f024fe6f88d8ae3d972486
NiPeGun <nipegun@gmail.com>
2023-02-07 13:37:22 +0100

    Update KScripts-CrearAlias.sh

--- KScripts-CrearAlias.sh
+++ KScripts-CrearAlias.sh
@@ -5,20 +5,25 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------
+# ----------
 #  Script de NiPeGun para crear los alias de los k-scripts 
-#-----------------------------------------------------------
+# ----------
 
 ColorVerde="\033[1;32m"
 FinColor="\033[0m"
 
 echo ""
-echo -e "${ColorVerde}Creando alias para los k-scripts...${FinColor}"
+echo -e "${ColorVerde}  Creando alias para los k-scripts...${FinColor}"
 echo ""
 
-ln -s /root/scripts/k-scripts/Externos/VelocidadDeInternet.sh              /root/scripts/d-scripts/Alias/vdi
+ln -s ~/scripts/k-scripts/SistemaOperativo-Actualizar.sh            ~/scripts/k-scripts/Alias/aso
+ln -s ~/scripts/k-scripts/SistemaOperativo-ActualizarYApagar.sh     ~/scripts/k-scripts/Alias/asoya
+ln -s ~/scripts/k-scripts/SistemaOperativo-ActualizarYReiniciar.sh  ~/scripts/k-scripts/Alias/asoyr
+ln -s ~/scripts/k-scripts/SistemaOperativo-Apagar.sh                ~/scripts/k-scripts/Alias/apso
+ln -s ~/scripts/k-scripts/SistemaOperativo-Reiniciar.sh             ~/scripts/k-scripts/Alias/rso
+
+ln -s ~/scripts/k-scripts/UScripts-Sincronizar.sh                    ~/scripts/k-scripts/Alias/sinus
 
 echo ""
 echo -e "${ColorVerde}Alias creados. Deberías poder ejecutar los k-scripts escribiendo el nombre de su alias.${FinColor}"
 echo ""
-
