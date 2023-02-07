d94c4585c44b3cf6ae2427c7b90934278a4759d9
NiPeGun <nipegun@gmail.com>
2023-02-07 13:39:24 +0100

    Create SistemaOperativo-Actualizar.sh

---
+++ SistemaOperativo-Actualizar.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+#  Script de NiPeGun para actualizar Kali
+#
+#  Ejecución remota:
+#  curl -s https://raw.githubusercontent.com/nipegun/k-scripts/main/SistemaOperativo-Actualizar.sh | bash
+# ----------
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+echo ""
+echo -e "${ColorVerde}  Iniciando el script de actualización de Kali...${FinColor}"
+echo ""
+
+echo ""
+echo "  Reparando permisos de la carpeta /tmp/ ...$"
+echo ""
+sudo chmod 1777 /tmp
+
+echo ""
+echo "  Ejecutando apt-get update..."
+echo ""
+sudo apt-get -y update
+
+echo ""
+echo "  Ejecutando apt-get -y upgrade..."
+echo ""
+sudo apt-get -y --allow-downgrades upgrade
+
+echo ""
+echo "  Ejecutando apt-get -y dist-upgrade..."
+echo ""
+sudo apt-get -y --allow-downgrades dist-upgrade
+
+echo ""
+echo "  Ejecutando apt-get -y autoremove..."
+echo ""
+sudo apt-get -y autoremove
+
+echo ""
+echo -e "${ColorVerde}  Script para actualizar el sistema operativo, finalizado.${FinColor}"
+echo ""
